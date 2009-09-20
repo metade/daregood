6ab8c8ab2b6555f6509ad75e5c3590b52cba5507
Jason Lee <jlsync@gmail.com>
2009-09-20 12:21:57 +0100

    100 deserving charities

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090919222122) do
+ActiveRecord::Schema.define(:version => 20090920111815) do
 
   create_table "attempts", :force => true do |t|
     t.integer  "challenge_id"
